9bccd5924ff0d251c1733b92e1e2b26553b80eb1
Juan Camilo Castano Patino <juancamilo372cp@live.com>
2022-09-25 10:51:48 -0500

    fix button current site selection

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { HostListener } from '@angular/core';
 
 @Component({
   selector: 'app-header',
@@ -9,15 +10,21 @@ export class HeaderComponent implements OnInit {
   constructor() {}
   // eslint-disable-next-line @angular-eslint/no-empty-lifecycle-method
   ngOnInit(): void {
-    let selecteditem = document.getElementById('user');
-    selecteditem!.className = 'mainitembuttonselected';
+    /* let selecteditem = document.getElementById('user');
+    selecteditem!.className = 'mainitembuttonselected';*/
+  }
+  @HostListener('window:load')
+  onPageLoaded() {
+    console.log(window.location.href.split('/')[4]);
+    this.select(window.location.href.split('/')[4]);
   }
   select(selected: string) {
-    let nonselecteditem = document.getElementsByClassName(
-      'mainitembuttonselected'
-    )[0] as HTMLButtonElement;
-    nonselecteditem.className = 'mainitembutton';
-    console.log(nonselecteditem);
+    let selecteditem1 = document.getElementById('user');
+    selecteditem1!.className = 'mainitembutton';
+    let selecteditem2 = document.getElementById('p2p');
+    selecteditem2!.className = 'mainitembutton';
+    let selecteditem3 = document.getElementById('trade');
+    selecteditem3!.className = 'mainitembutton';
     let selecteditem = document.getElementById(selected);
     selecteditem!.className = 'mainitembuttonselected';
   }
