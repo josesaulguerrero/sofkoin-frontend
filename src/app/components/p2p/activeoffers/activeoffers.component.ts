6d93dae3b25252ab504eecec503f26d5783bb71f
Juan Camilo Castano Patino <juancamilo372cp@live.com>
2022-09-27 13:04:35 -0500

    Active Offer working withoutUsernames

--- src/app/components/p2p/activeoffers/activeoffers.component.ts
+++ src/app/components/p2p/activeoffers/activeoffers.component.ts
@@ -3,6 +3,7 @@ import { ErrorModel } from 'src/app/models/errorModel';
 import { OfferModel } from 'src/app/models/offerModel';
 import { BetarequestService } from 'src/app/services/request/betarequest.service';
 import { StateService } from 'src/app/services/state/state.service';
+import { RequestService } from 'src/app/services/request/alpharequest.service';
 @Component({
   selector: 'app-activeoffers',
   templateUrl: './activeoffers.component.html',
@@ -11,7 +12,8 @@ import { StateService } from 'src/app/services/state/state.service';
 export class ActiveoffersComponent implements OnInit {
   constructor(
     private state: StateService,
-    private betarequest: BetarequestService
+    private betarequest: BetarequestService,
+    private alphaservice: RequestService
   ) {}
 
   offers: OfferModel[] = [];
@@ -44,5 +46,115 @@ export class ActiveoffersComponent implements OnInit {
         }
       },
     });
+  }
+  deleteoffer(offer: OfferModel) {
+    this.alphaservice
+      .deleteOfferMethod(
+        {
+          marketId: this.state.market.value.marketId,
+          offerId: offer.offerId,
+        },
+        localStorage.getItem('token')!
+      )
+      .subscribe({
+        next: (response) => {
+          console.log(response);
+          this.offers = this.offers.filter(function (e) {
+            return e.offerId !== offer.offerId;
+          });
+        },
+        error: (err: ErrorModel) => {
+          if (
+            err.error.errorMessage === null ||
+            err.error.errorMessage === undefined
+          ) {
+            alert('Something went wrong with the market');
+          } else {
+            alert(err.error.errorMessage);
+          }
+        },
+      });
+  }
+  showOffer(offer: OfferModel): Boolean {
+    let currentUserId = localStorage.getItem('userId')!;
+
+    if (
+      currentUserId === offer.targetAudienceId ||
+      currentUserId === offer.publisherId ||
+      offer.targetAudienceId === '-'
+    ) {
+      return true;
+    }
+    return false;
+  }
+  changeOfferclass(offer: OfferModel): Boolean {
+    let currentUserId = localStorage.getItem('userId')!;
+    let currentoffer = document.getElementById(offer.offerId);
+    if (currentUserId === offer.targetAudienceId) {
+      console.log(currentoffer);
+      currentoffer!.className = 'offerforme';
+      console.log(currentoffer);
+      return true;
+    }
+    if (currentUserId === offer.publisherId) {
+      console.log(currentoffer);
+      currentoffer!.className = 'mineoffer';
+      console.log(currentoffer);
+      return true;
+    }
+    if (offer.targetAudienceId === '-') {
+      return true;
+    }
+    return false;
+  }
+  showasmine(publisherId: string): Boolean {
+    let currentUserId = localStorage.getItem('userId')!;
+    if (currentUserId === publisherId) {
+      return true;
+    }
+    return false;
+  }
+  showasgeneral(offer: OfferModel): Boolean {
+    let currentUserId = localStorage.getItem('userId')!;
+    if (offer.targetAudienceId === '-' && offer.publisherId !== currentUserId) {
+      return true;
+    }
+    return false;
+  }
+  showasforme(publisherId: string): Boolean {
+    let currentUserId = localStorage.getItem('userId')!;
+    if (currentUserId === publisherId) {
+      return true;
+    }
+    return false;
+  }
+  buyoffer(offer: OfferModel) {
+    this.alphaservice
+      .p2pTransactionMethod(
+        {
+          buyerId: localStorage.getItem('userId')!,
+          marketId: this.state.market.value.marketId,
+          offerId: offer.offerId,
+        },
+        localStorage.getItem('token')!
+      )
+      .subscribe({
+        next: (response) => {
+          console.log(response);
+          this.offers = this.offers.filter(function (e) {
+            return e.offerId !== offer.offerId;
+          });
+        },
+        error: (err: ErrorModel) => {
+          if (
+            err.error.errorMessage === null ||
+            err.error.errorMessage === undefined
+          ) {
+            alert('Something went wrong with the market');
+          } else {
+            alert(err.error.errorMessage);
+          }
+        },
+      });
   }
 }
