78b9a5b616807e0b987c7d5a9776b82ab3cc0d23
Carlos Romero <caaromerogi@unal.edu.co>
2022-09-27 15:02:54 -0500

    Message offer accepted working, is missing change the status

--- src/app/components/trade/sell/sell.component.ts
+++ src/app/components/trade/sell/sell.component.ts
@@ -26,13 +26,8 @@ export class SellComponent implements OnInit {
   cryptoSelected: string = '--';
   cryptoSelectedTotalPrice: number = 0;
   cashAvailable?: number;
-<<<<<<< HEAD
   userCryptos?: UserCryptosList[] = [];
   isLoaded: boolean = false;
-=======
-  userCryptos: UserCryptosList[] = [];
-  isLoaded: boolean = true;
->>>>>>> a11ffa9d00b010fa936842a0e5205ceb8be737ca
 
   availableCryptos?: UserCryptosList[];
   cryptos?: CryptoPriceModel[];
@@ -124,7 +119,6 @@ export class SellComponent implements OnInit {
     }
 
     //TODO: ACTUALIZAR EL ESTADO DEL USUARIO/MOSTRAR USD
-    console.log(command);
   }
 
   validation(): boolean {
