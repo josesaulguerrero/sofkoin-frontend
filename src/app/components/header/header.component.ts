504bb42cea02709d26f5ab167bd308e0223364c3
Juan Camilo Castano Patino <juancamilo372cp@live.com>
2022-09-27 15:48:01 -0500

    Pie Chart Added, Header/sidebar menu fixed

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { HostListener } from '@angular/core';
 import { AuthService } from 'src/app/services/auth/auth.service';
 import { RequestService } from 'src/app/services/request/alpharequest.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-header',
@@ -10,6 +11,7 @@ import { RequestService } from 'src/app/services/request/alpharequest.service';
 })
 export class HeaderComponent implements OnInit {
   constructor(
+    private router: Router,
     private auth: AuthService,
     private alphaRequest: RequestService
   ) {}
@@ -20,19 +22,30 @@ export class HeaderComponent implements OnInit {
   }
   @HostListener('window:load')
   onPageLoaded() {
-    this.select(window.location.href.split('/')[4]);
+    /* let selecteditem = document.getElementById('user');
+    console.log(selecteditem);
+    selecteditem!.className = 'mainitembuttonselected';
+    this.select(window.location.href.split('/')[4]);*/
   }
   select(selected: string) {
-    let selecteditem1 = document.getElementById('user');
-    selecteditem1!.className = 'mainitembutton';
-    let selecteditem2 = document.getElementById('p2p');
-    selecteditem2!.className = 'mainitembutton';
-    let selecteditem3 = document.getElementById('trade');
-    selecteditem3!.className = 'mainitembutton';
-    let selecteditem = document.getElementById(selected);
-    selecteditem!.className = 'mainitembuttonselected';
+    if (window.location.href.split('/')[4] !== selected) {
+      let selecteditem1 = document.getElementById('user');
+      selecteditem1!.className = 'mainitembutton';
+      let selecteditem2 = document.getElementById('p2p');
+      selecteditem2!.className = 'mainitembutton';
+      let selecteditem3 = document.getElementById('trade');
+      selecteditem3!.className = 'mainitembutton';
+      let selecteditem = document.getElementById(selected);
+      selecteditem!.className = 'mainitembuttonselected';
+      this.router.navigateByUrl('/main/' + selected);
+    }
   }
-
+  firstload() {
+    if (window.location.href.split('/')[4] === 'user') {
+      let selecteditem = document.getElementById('user');
+      selecteditem!.className = 'mainitembuttonselected';
+    }
+  }
   logOut() {
     this.alphaRequest
       .logout(
--- src/app/models/CryptoUsrList.ts
+++ src/app/models/CryptoUsrList.ts
@@ -1,4 +1,5 @@
 export type UserCryptosList = {
   symbol: string;
   amount: number;
+  priceUsd: number;
 };
